939810bf1440414ec1ea3cf26d09c7fe16fef45b
Evgeny Bochkarev <45517037+djrebrow@users.noreply.github.com>
2025-08-25 17:57:21 +0200

    Delete rules directory

--- rules/de.js
+++
@@ -1,9 +0,0 @@
-export default `
-<h3>Spielregeln</h3>
-<ol>
-  <li>Raum erstellen oder per Code beitreten.</li>
-  <li>Ein Spieler ist Spion, die anderen Bürger.</li>
-  <li>Diskussion: Hinweise geben, Wort nicht verraten.</li>
-  <li>Abstimmung: Wer ist der Spion?</li>
-  <li>Punkte: Bürger +1 bei Treffer, sonst +1 für den Spion.</li>
-</ol>`;
--- rules/en.js
+++
@@ -1,9 +0,0 @@
-export default `
-<h3>Rules</h3>
-<ol>
-  <li>Create a room or join via code.</li>
-  <li>One player is the Spy, others are Citizens.</li>
-  <li>Discussion: give hints, don't say the word.</li>
-  <li>Vote: who is the Spy?</li>
-  <li>Scoring: +1 to Citizens if correct, otherwise +1 to Spy.</li>
-</ol>`;
